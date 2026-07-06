8d4848e780c5394bcbf21a0e72a1e94e59eda589
cjrackley <cjrackley@uncg.edu>
2026-07-06 10:28:56 -0400

    added files for backend api

---
+++ backend-api/src/main/java/com/CSC340/MealPrep_Match/MealPrepMatchApplication.java
@@ -0,0 +1,13 @@
+package com.CSC340.MealPrep_Match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPrepMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPrepMatchApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/com/CSC340/MealPrep_Match/MealPrepMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSC340.MealPrep_Match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPrepMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
