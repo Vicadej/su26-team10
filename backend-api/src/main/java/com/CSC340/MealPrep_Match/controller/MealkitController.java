66e6212eacda611f302c325026619b43a4a88b0b
cjrackley <cjrackley@uncg.edu>
2026-07-10 23:16:08 -0400

    Added mealkit API

---
+++ backend-api/src/main/java/com/CSC340/MealPrep_Match/controller/MealkitController.java
@@ -0,0 +1,76 @@
+package com.CSC340.MealPrep_Match.controller;
+
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.CSC340.MealPrep_Match.entity.Mealkit;
+import com.CSC340.MealPrep_Match.entity.Mealplan;
+import com.CSC340.MealPrep_Match.service.MealkitService;
+
+@RestController
+@RequestMapping("/api/mealkits")
+public class MealkitController {
+    private MealkitService mealkitService;
+
+    public MealkitController(MealkitService mealkit) {
+        this.mealkitService = mealkit;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Mealkit>> getAllMealkits() {
+        List<Mealkit> mealkits = mealkitService.getAllMealkits();
+        return ResponseEntity.ok(mealkits);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Mealkit> getMealkitById(@PathVariable Long id) {
+        Mealkit mealkit = mealkitService.getMealkitById(id);
+        if (mealkit == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(mealkit);
+    }
+
+    @GetMapping("/provider/providerId")
+    public ResponseEntity<List<Mealkit>> getMealkitByProviderId(@PathVariable Long providerId) {
+        List<Mealkit> mealkits = mealkitService.getMealkitByProviderId(providerId);
+        return ResponseEntity.ok(mealkits); 
+    }
+
+    @GetMapping("/catgory/{category}")
+    public ResponseEntity<List<Mealkit>> getMealkitsByCategory(@PathVariable String category) {
+        List<Mealkit> mealkits = mealkitService.getMealkitsByCategory(category);
+        return ResponseEntity.ok(mealkits);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Mealkit>> searchMealkitsByTitle(@RequestParam String query) {
+        List<Mealkit> mealkits = mealkitService.searchMealkitsByTitle(query);
+        return ResponseEntity.ok(mealkits);
+    }
+
+    @PostMapping
+    public ResponseEntity<Mealkit> createMealkit(@RequestBody Mealkit mealkit) {
+        Mealkit createdMealkit = mealkitService.createMealkit(mealkit);
+        return ResponseEntity.ok(createdMealkit);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Mealkit> updateMealkit(@PathVariable Long id, @RequestBody Mealkit mealkit) {
+        Mealkit updatedMealkit = mealkitService.updateMealkit(id, mealkit);
+        if (updatedMealkit == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedMealkit);
+    }
+}
---
+++ backend-api/src/main/java/com/CSC340/MealPrep_Match/entity/Mealkit.java
@@ -0,0 +1,60 @@
+package com.CSC340.MealPrep_Match.entity;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "mealkit")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mealkit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"mealkit"})
+    @JoinColumn(nullable = false)
+    private Provider provider;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String deliveryFrequency;
+
+    @Column(nullable = false)
+    private String description;
+
+    private String category;
+
+    @OneToOne(mappedBy = "mealkit")
+    @JsonIgnore
+    private List<Mealplan> mealplan;
+
+    public Mealkit(Provider provider, String title, String deliveryFrequency, String description, String category) {
+        this.provider = provider;
+        this.title = title;
+        this.deliveryFrequency = deliveryFrequency;
+        this.description = description;
+        this.category = category;
+    }
+    
+}
